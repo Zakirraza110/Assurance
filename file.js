106620696ebcc77acb40c808cbfa3c5806ad3394
Zakir <124083856+Zakirraza110@users.noreply.github.com>
2024-05-09 22:40:01 -0700

    files added

---
+++ file.js
@@ -0,0 +1,8 @@
+function ham(){
+  document.getElementById('hamberger').style.display="block";
+  document.getElementById('list').style.display="block"
+}
+
+function hamreturn(){
+  document.getElementById('hamberger').style.display="none";
+}
